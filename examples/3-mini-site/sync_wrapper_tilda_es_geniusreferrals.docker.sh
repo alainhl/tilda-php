10514292e31608e76f77678a664067e711f5b15d
Alain Hernandez <alainhl@gmail.com>
2024-06-23 17:24:38 -0400

    adding new wrappers for support, soporte and es tilda sites. This wrappers will run the sync.php file.

---
+++ examples/3-mini-site/sync_wrapper_tilda_es_geniusreferrals.docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "----------> Starting running sync.php at $(date)";
+#/usr/local/bin/php -d memory_limit=1024m /var/www/tilda-en-geniusreferrals/examples/3-mini-site/sync.php
+/usr/local/bin/php -d memory_limit=1024m /var/www/tilda-es-geniusreferrals/examples/3-mini-site/sync.php > /proc/1/fd/1 2>/proc/1/fd/2
+echo "----------> Ending running sync.php at $(date)";
+
