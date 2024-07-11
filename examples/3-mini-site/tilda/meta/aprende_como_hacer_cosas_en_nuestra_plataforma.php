332bd6b2c285e8ead77bacaee3c9147f7de577fc
Jenkins <jenkins@geniusreferrals.com>
2024-07-11 01:42:00 +0000

    adding new pages

---
+++ examples/3-mini-site/tilda/meta/aprende_como_hacer_cosas_en_nuestra_plataforma.php
@@ -0,0 +1,1 @@
+<?php return include "page45613827.php"; ?>
---
+++ examples/3-mini-site/tilda/meta/page45613827.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'id' => '45613827',
+    'title' => "Aprenda todo sobre programas de referidos",
+    'alias' => 'aprende_como_hacer_cosas_en_nuestra_plataforma',
+    'descr' => "Aprenda fácilmente cómo implementar su programa de referidos con nuestra plataforma, nuestros años de experiencia en la industria te permitirán lanzar tu programa inmediatamente.",
+    'img' => 'https://static.tildacdn.com/tild3662-3831-4339-b131-376662636164/image.png',
+    'featureimg' => '',
+    'socnetimg' => '',
+    'needsync' => '0'
+);
+?>
