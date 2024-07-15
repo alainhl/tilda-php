1b99245f9800b64a4f63d0138dfb0620e462cf83
Jenkins <jenkins@geniusreferrals.com>
2024-07-15 18:38:00 +0000

    Adding new pages

---
+++ examples/3-mini-site/tilda/meta/page45627907.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'id' => '45627907',
+    'title' => " Proceso de Aprobación de Bonificación | Genius Referrals",
+    'alias' => 'proceso_de_aprobacion_de_bonificacion',
+    'descr' => "Aprenda a Activar y Probar el Proceso de Aprobación de Bonos Para Administrar Manualmente las Recompensas.",
+    'img' => 'https://static.tildacdn.com/tild3163-6438-4261-b332-306138343863/bonus.png',
+    'featureimg' => '',
+    'socnetimg' => '',
+    'needsync' => '0'
+);
+?>
---
+++ examples/3-mini-site/tilda/meta/proceso_de_aprobacion_de_bonificacion.php
@@ -0,0 +1,1 @@
+<?php return include "page45627907.php"; ?>
