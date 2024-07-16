cab138f40ccf9e832657f3c5c9d05d4a39aa86e4
Jenkins <jenkins@geniusreferrals.com>
2024-07-16 18:00:10 +0000

    Adding new pages

---
+++ examples/3-mini-site/tilda/meta/footer.php
@@ -0,0 +1,1 @@
+<?php return include "page8713899.php"; ?>
---
+++ examples/3-mini-site/tilda/meta/page8713899.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'id' => '8713899',
+    'title' => "Footer",
+    'alias' => 'footer',
+    'descr' => "",
+    'img' => '',
+    'featureimg' => '',
+    'socnetimg' => '',
+    'needsync' => '0'
+);
+?>
