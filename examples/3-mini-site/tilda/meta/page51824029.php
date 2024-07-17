48137b615c1b9961bcf56412fb07e70f0e4e194b
Jenkins <jenkins@geniusreferrals.com>
2024-07-17 18:00:08 +0000

    Adding new pages

--- examples/3-mini-site/tilda/meta/page51824029.php
+++ examples/3-mini-site/tilda/meta/page51824029.php
@@ -2,7 +2,7 @@
 return array(
     'id' => '51824029',
     'title' => "Copy of Request a Demo | Genius Referrals",
-    'alias' => '',
+    'alias' => 'new-demo-page',
     'descr' => "Schedule your personalized demo and learn how our software can help you grow your business with referrals.",
     'img' => '',
     'featureimg' => '',
