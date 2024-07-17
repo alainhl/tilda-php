1520bdfb50c059bb04daf29413bde99334f09875
Jenkins <jenkins@geniusreferrals.com>
2024-07-17 18:00:14 +0000

    Adding new pages

---
+++ examples/3-mini-site/tilda/meta/page49671041.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'id' => '49671041',
+    'title' => "Unlock the Power of Referrals with Genius Referrals - The Ultimate Referral Program Software",
+    'alias' => '',
+    'descr' => "Looking to supercharge your growth and boost your sales? Genius Referrals is the ultimate referral program software that helps you harness the power of word-of-mouth marketing. Our cutting-edge technology streamlines the referral process, engages your customers, and maximizes your ROI. Discover how Genius Referrals can transform your business today.",
+    'img' => 'https://static.tildacdn.com/tild3962-6663-4166-b330-653338353562/banner.jpg',
+    'featureimg' => '',
+    'socnetimg' => '',
+    'needsync' => '0'
+);
+?>
