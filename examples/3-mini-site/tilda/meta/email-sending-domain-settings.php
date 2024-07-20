acfb89fc94ace4314c1fc15ef38b83706089fc7b
Jenkins <jenkins@geniusreferrals.com>
2024-07-20 18:00:22 +0000

    Adding new pages

---
+++ examples/3-mini-site/tilda/meta/email-sending-domain-settings.php
@@ -0,0 +1,1 @@
+<?php return include "page43202204.php"; ?>
---
+++ examples/3-mini-site/tilda/meta/page43202204.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'id' => '43202204',
+    'title' => "Email Sending Domain Settings | Genius Referrals",
+    'alias' => 'email-sending-domain-settings',
+    'descr' => "Learn How To Customize the From and Reply-To email addresses used for your program emails.",
+    'img' => 'https://static.tildacdn.com/tild3163-6438-4261-b332-306138343863/bonus.png',
+    'featureimg' => '',
+    'socnetimg' => '',
+    'needsync' => '0'
+);
+?>
