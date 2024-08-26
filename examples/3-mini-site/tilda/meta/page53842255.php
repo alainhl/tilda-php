48ac8790554d554f09671565d816266ba5b0cc98
Jenkins <jenkins@geniusreferrals.com>
2024-08-26 18:00:17 +0000

    Adding new pages

---
+++ examples/3-mini-site/tilda/meta/page53842255.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'id' => '53842255',
+    'title' => "",
+    'alias' => '',
+    'descr' => "",
+    'img' => '',
+    'featureimg' => '',
+    'socnetimg' => '',
+    'needsync' => '1'
+);
+?>
